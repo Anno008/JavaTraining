223027484a12036dea42ba21c4e32c173384c705
Aleksa Jovicic <aleksa.j@outlook.com>
2017-03-30 21:38:50 +0200

    - Initial commit together with the first problem related to working with arrays

---
+++ ThirdLesson-WorkingWithArrays/src/problemOne/ProblemOne.java
@@ -0,0 +1,26 @@
+package problemOne;
+
+public class ProblemOne {
+	public static void main(String[] args) {
+
+		int amountToCheckout = 14350;
+		int[][] availableMoneyAmount = { { 2000, 13 }, { 1000, 5 }, { 500, 2 }, { 100, 4 }, { 50, 55 } };
+		String output = "";
+
+		for (int i = 0; i < availableMoneyAmount.length; i++) {
+			int count = 0;
+			for (int j = 0; j < availableMoneyAmount[i].length; j++) {
+				while (availableMoneyAmount[i][1] > 0) { 
+					if (amountToCheckout <= 0 || amountToCheckout < availableMoneyAmount[i][0])
+						break;
+					amountToCheckout -= availableMoneyAmount[i][0];
+					availableMoneyAmount[i][1] -= 1;
+					count++;
+				}
+			}
+			output += "Checked out " + count + " bills with the value of: " + availableMoneyAmount[i][0] + "\n";
+		}
+		
+		System.out.println(output);
+	}
+}
