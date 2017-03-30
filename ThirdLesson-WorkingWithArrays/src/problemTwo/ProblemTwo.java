d1cd5922fcb27930a8a17fc2a221572b0e70c684
Aleksa Jovicic <aleksa.j@outlook.com>
2017-03-30 21:52:40 +0200

    - Added problemTwo implementation

---
+++ ThirdLesson-WorkingWithArrays/src/problemTwo/ProblemTwo.java
@@ -0,0 +1,22 @@
+package problemTwo;
+
+public class ProblemTwo {
+	public static void main(String[] args) {
+		int[] availableNumbers = { 1, 23, 5, 6, 34, 54, 65, 888, 65, 45, 4, 7, 15, 54 };
+
+		int numberToFind = 54;
+		int count = 0;
+		String index = "";
+
+		for (int i = 0; i < availableNumbers.length; i++) {
+			if (availableNumbers[i] != numberToFind)
+				continue;
+			index += i + " ";
+			count++;
+		}
+		
+		System.out.println(count > 0 ? "Number " + numberToFind + " exists in the array and appears " + count + " times, at indexes: " + index :
+									   "Number " + numberToFind + " doesn't exist in the array");
+
+	}
+}
