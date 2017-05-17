96d089f807a9145465a996a1692499b0d58f8fa7
Aleksa Jovicic <aleksa.j@outlook.com>
2017-05-17 18:59:03 +0200

    Fixed an eclipse warning

--- 13.LessonThirteen-Tree/src/tree/Tree.java
+++ 13.LessonThirteen-Tree/src/tree/Tree.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Tree<E> {
 		private Node<E> root;
 		
-		public Node add(Node<E> parent, E data) {
+		public Node<E> add(Node<E> parent, E data) {
 			Node<E> newNode = new Node<>(data, parent);
 			
 			if (parent == null) 
