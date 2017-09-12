c563486de7efec0bb33471f8341bdb387fc72593
Aleksa Jovicic <aleksa.j@outlook.com>
2017-09-12 15:41:57 +0200

    Updated App class

--- 29.LessonTwentyNine-StudentsMavenSpring/src/main/java/studentsMavenSpring/App.java
+++ 29.LessonTwentyNine-StudentsMavenSpring/src/main/java/studentsMavenSpring/App.java
@@ -1,13 +1,14 @@
 package studentsMavenSpring;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import studentsMavenSpring.config.AppConfig;
+
+public class App {
+	public static void main(String[] args) {
+		@SuppressWarnings({ "unused", "resource" })
+		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		System.out.println("Hello World!");
+	}
 }
