c1176754a19a5e1728c3d4fe87cc8c23a50f3954
Aleksa Jovicic <aleksa.j@outlook.com>
2017-09-12 16:37:04 +0200

    Added student repository

---
+++ 29.LessonTwentyNine-StudentsMavenSpring/src/main/java/studentsMavenSpring/repository/StudentRepository.java
@@ -0,0 +1,112 @@
+package studentsMavenSpring.repository;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import studentsMavenSpring.model.Student;
+
+@Component("studentRepository")
+public class StudentRepository {
+	private List<Student> students = new ArrayList<Student>();
+	private String path = "src/main/java/studentsMavenSpring/students.txt";
+
+	public List<Student> findAll() {
+		try {
+			return readStudents();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public Student find(int id) {
+		try {
+			for (Student student : readStudents())
+				if (student.getId() == id)
+					return student;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public void save(Student student) {
+		Student existingStudent = find(student.getId());
+		if (existingStudent == null)
+			students.add(student);
+		else {
+			student.setName(student.getName());
+			student.setSurname(student.getSurname());
+		}
+		try {
+			saveStudents(students);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void delete(int id) {
+		Iterator<Student> studentsIterator = students.iterator();
+		while (studentsIterator.hasNext()) {
+			if (studentsIterator.next().getId() == id) {
+				studentsIterator.remove();
+				return;
+			}
+		}
+		try {
+			saveStudents(students);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private Student string2Student(String row) throws IOException {
+		String[] s = row.split(",");
+		return new Student(Integer.parseInt(s[0]), s[1], s[2]);
+	}
+
+	private String student2String(Student st) {
+		return st.getId() + "," + st.getName() + "," + st.getSurname();
+	}
+
+	private List<Student> readStudents() throws IOException {
+		BufferedReader inputStream = null;
+		List<Student> students = null;
+		File yourFile = new File(path);
+		yourFile.createNewFile();
+		try {
+			inputStream = new BufferedReader(new FileReader(path));
+			students = new ArrayList<Student>();
+			String l;
+			while ((l = inputStream.readLine()) != null) {
+				students.add(string2Student(l));
+			}
+			return students;
+		} finally {
+			if (inputStream != null) {
+				inputStream.close();
+			}
+		}
+	}
+
+	private void saveStudents(List<Student> students) throws IOException {
+		PrintWriter outputStream = null;
+		try {
+			outputStream = new PrintWriter(new FileWriter(path));
+			for (Student student : students)
+				outputStream.println(student2String(student));
+		} finally {
+			if (outputStream != null)
+				outputStream.close();
+		}
+	}
+}
