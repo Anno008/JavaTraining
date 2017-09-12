2a95bec2a0f95c884625030001d9bdbfc4b962dd
Aleksa Jovicic <aleksa.j@outlook.com>
2017-09-12 15:06:49 +0200

    Created a new Maven project for students management

---
+++ 29.LessonTwentyNine-StudentsMavenSpring/src/main/java/StudentsMavenSpring/LessonTwentyNine_StudentsMavenSpring/App.java
@@ -0,0 +1,13 @@
+package StudentsMavenSpring.LessonTwentyNine_StudentsMavenSpring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
