0b5db5d5074a2d75fc8e7df8b1f2f0cf335c21e3
Aleksa Jovicic <aleksa.j@outlook.com>
2017-09-12 17:16:00 +0200

    Added exam service

---
+++ 29.LessonTwentyNine-StudentsMavenSpring/src/main/java/studentsMavenSpring/service/ExamService.java
@@ -0,0 +1,19 @@
+package studentsMavenSpring.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import studentsMavenSpring.model.Exam;
+import studentsMavenSpring.repository.ExamRepository;
+
+@Component
+public class ExamService {
+	@Autowired
+	private ExamRepository examRepository;
+
+	public List<Exam> findAll(){
+		return examRepository.findAll();
+	}
+}
