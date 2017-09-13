451c2b5e7d943d2514018ccef60cb836680a7b83
Aleksa Jovicic <aleksa.j@outlook.com>
2017-09-13 22:31:55 +0200

    Added User class

---
+++ 29.LessonTwentyNine-CountriesMavenSpring/src/main/java/countriesMavenSpring/model/User.java
@@ -0,0 +1,37 @@
+package countriesMavenSpring.model;
+
+public class User {
+	private String userName;
+	private String password;
+	private boolean isAdmin;
+
+	public User(String userName, String password, boolean isAdmin) {
+		this.userName = userName;
+		this.password = password;
+		this.isAdmin = isAdmin;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+}
