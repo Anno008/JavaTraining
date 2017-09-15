1844f8d495f7c19a0b3f173b3b87f5b287f671b4
Aleksa Jovicic <aleksa.j@outlook.com>
2017-09-15 10:46:50 +0200

    Created Country Service

---
+++ 30.LessonThirty-MavenApacheTomcatCountriesRestApplication/src/main/java/countries/controller/CountryService.java
@@ -0,0 +1,33 @@
+package countries.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import countries.model.Country;
+import countries.repository.CountryRepository;
+
+@Component
+public class CountryService {
+	private CountryRepository countryRepository;
+
+	public List<Country> getAll() {
+		return countryRepository.getAll();
+	}
+
+	public Country get(int id) {
+		return countryRepository.get(id);
+	}
+
+	public void save(Country country) {
+		countryRepository.save(country);
+	}
+
+	public void delete(int id) {
+		countryRepository.delete(id);
+	}
+
+	public List<Country> findByName(String name) {
+		return countryRepository.findByName(name);
+	}
+}
