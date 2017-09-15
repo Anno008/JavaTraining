4d134070b2ef8ff973c03a4965ed195fbe324339
Aleksa Jovicic <aleksa.j@outlook.com>
2017-09-15 10:56:04 +0200

    Added City Service

---
+++ 30.LessonThirty-MavenApacheTomcatCountriesRestApplication/src/main/java/countries/controller/CityService.java
@@ -0,0 +1,33 @@
+package countries.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import countries.model.City;
+import countries.repository.CityRepository;
+
+@Component
+public class CityService {
+	private CityRepository cityRepository;
+
+	public List<City> getAll() {
+		return cityRepository.getAll();
+	}
+
+	public City get(int id) {
+		return cityRepository.get(id);
+	}
+
+	public void save(City city) {
+		cityRepository.save(city);
+	}
+
+	public void delete(int id) {
+		cityRepository.delete(id);
+	}
+
+	public List<City> findByName(String name) {
+		return cityRepository.findByName(name);
+	}
+}
