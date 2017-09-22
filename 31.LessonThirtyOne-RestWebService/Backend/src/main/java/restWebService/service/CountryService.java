07cc15f20c8bcc7ec5d4395a6ae436ef5e4c3a00
Aleksa Jovicic <aleksa.j@outlook.com>
2017-09-22 19:56:20 +0200

    Created country service

---
+++ 31.LessonThirtyOne-RestWebService/Backend/src/main/java/restWebService/service/CountryService.java
@@ -0,0 +1,43 @@
+package restWebService.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import restWebService.model.Country;
+import restWebService.repository.CountryRepository;
+
+@Component
+public class CountryService {
+	@Autowired
+	private CountryRepository countryRepository;
+
+	public List<Country> getAll() {
+		return countryRepository.getAll();
+	}
+
+	public Country get(int id) {
+		return countryRepository.get(id);
+	}
+
+	public List<Country> getAllWithPopulationHigher(int population) {
+		return countryRepository.getAllWithPopulationHigher(population);
+	}
+
+	public List<Country> getAllWithPopulationLower(int population) {
+		return countryRepository.getAllWithPopulationLower(population);
+	}
+
+	public void save(Country country) {
+		countryRepository.save(country);
+	}
+
+	public void delete(int id) {
+		countryRepository.delete(id);
+	}
+
+	public List<Country> findByName(String name) {
+		return countryRepository.findByName(name);
+	}
+}
