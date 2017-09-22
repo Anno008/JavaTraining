678dcbf06e4dd4be937264aa6397b7792ce3eece
Aleksa Jovicic <aleksa.j@outlook.com>
2017-09-22 19:39:09 +0200

    Created country repository

---
+++ 31.LessonThirtyOne-RestWebService/Backend/src/main/java/restWebService/repository/CountryRepository.java
@@ -0,0 +1,59 @@
+package restWebService.repository;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+
+import restWebService.model.Country;
+
+@Component("countryRepository")
+public class CountryRepository {
+	private List<Country> countries;
+
+	public CountryRepository() {
+		countries = Arrays.asList(
+				new Country(1, "Serbia", 7000000), 
+				new Country(2, "Netherlands", 17000000),
+				new Country(3, "Austria", 8700000));
+	}
+
+	public List<Country> getAll() {
+		return countries;
+	}
+
+	public Country get(int id) {
+		return countries.stream().filter(c -> c.getId() == id).findFirst().get();
+	}
+
+	public Country save(Country country) {
+		Country existingCountry = get(country.getId());
+		if (existingCountry == null) {
+			country.setId(Collections.max(countries, Comparator.comparing(c -> c.getId())).getId() + 1);
+			countries.add(country);
+		} else
+			existingCountry.setName(country.getName());
+
+		return country;
+	}
+
+	public void delete(int id) {
+		countries.remove(countries.stream().filter(c -> c.getId() == id).findFirst().get());
+	}
+
+	public List<Country> findByName(String name) {
+		return countries.stream().filter(c -> c.getName().toLowerCase().contains(name.toLowerCase())).collect(Collectors.toList());
+	}
+
+	public List<Country> getAllWithPopulationHigher(int population) {
+		return countries.stream().filter(c -> c.getPopulation() >= population).collect(Collectors.toList());
+	}
+
+	public List<Country> getAllWithPopulationLower(int population) {
+		return countries.stream().filter(c -> c.getPopulation() <= population).collect(Collectors.toList());
+	}
+}
