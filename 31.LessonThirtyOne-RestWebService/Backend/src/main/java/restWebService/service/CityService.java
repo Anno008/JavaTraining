2cf1c442a22360fe2d660a9bec65454e58a3dae1
Aleksa Jovicic <aleksa.j@outlook.com>
2017-09-22 20:07:26 +0200

    Created city service

---
+++ 31.LessonThirtyOne-RestWebService/Backend/src/main/java/restWebService/service/CityService.java
@@ -0,0 +1,39 @@
+package restWebService.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import restWebService.model.City;
+import restWebService.repository.CityRepository;
+
+@Component
+public class CityService {
+	@Autowired
+	private CityRepository cityRepository;
+
+	public List<City> getAll() {
+		return cityRepository.getAll();
+	}
+
+	public City get(int id) {
+		return cityRepository.get(id);
+	}
+	
+	public List<City> getByCountry(int id){
+		return cityRepository.getByCountry(id);
+	}
+
+	public void save(City city) {
+		cityRepository.save(city);
+	}
+
+	public void delete(int id) {
+		cityRepository.delete(id);
+	}
+
+	public List<City> findByName(String name) {
+		return cityRepository.findByName(name);
+	}
+}
