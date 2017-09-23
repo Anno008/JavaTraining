acbfa6a657fd967566dfefc1a29782e2524e97f4
Aleksa Jovicic <aleksa.j@outlook.com>
2017-09-23 16:07:35 +0200

    Created an empty react application

---
+++ 31.LessonThirtyOne-RestWebService/Frontend/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+    devtool: "eval",
+
+    // entry point of our application, within the `src` directory (which we add to resolve.modules below):
+    entry: [
+        "babel-polyfill", "index.jsx"
+    ],
+
+    output: {
+        filename: "app.js",
+        publicPath: "build",
+        path: path.resolve("build")
+    },
+
+    // configure the dev server to run 
+    devServer: {
+        port: 3000,
+        historyApiFallback: true,
+        inline: true,
+    },
+
+    // tell Webpack to load Java Script files
+    resolve: {
+        // Look for modules in .js(x)
+        extensions: [".js", ".jsx"],
+
+        // add 'src' to the modules, so that when you import files you can do so with 'src' as the relative route
+        modules: ["src", "node_modules"],
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                include: path.resolve(__dirname, "src"),
+                exclude: /(node_modules)/,
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            "presets": ["es2016", "react"]
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+}
