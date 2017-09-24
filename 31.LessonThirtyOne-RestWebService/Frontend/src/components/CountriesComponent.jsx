857964bbe15396b7bb18ecdd896d09fae243bace
Aleksa Jovicic <aleksa.j@outlook.com>
2017-09-24 14:17:50 +0200

    Added a text box and mapped dispatch to props to get countries by name on input field text changed

--- 31.LessonThirtyOne-RestWebService/Frontend/src/components/CountriesComponent.jsx
+++ 31.LessonThirtyOne-RestWebService/Frontend/src/components/CountriesComponent.jsx
@@ -3,13 +3,16 @@ import { connect } from "react-redux";
 import CountryComponent from "./CountryComponent";
 import "bulma/css/bulma.css";
 import "../css/cities.css";
+import { fetchCountriesByName } from "../actions/CountryActions";
 
 const CountriesComponent = (props) => {
     return (
         <div className="container">
+            <input className="input" type="text" placeholder="Country name"
+                onChange={(e) => props.fetchCountriesByName(e.target.value)} />
             {props.countriesState.data ? props.countriesState.data.map(c =>
                 <div className="column is-half float" key={c.id}>
-                        <CountryComponent country={c} />
+                    <CountryComponent country={c} />
                 </div>
             ) : ""}
         </div>
@@ -21,6 +24,7 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
+    fetchCountriesByName: (name) => dispatch(fetchCountriesByName(name)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CountriesComponent);
