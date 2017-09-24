9a089e1764bc2237f88b6097eb95831d0aeeba26
Aleksa Jovicic <aleksa.j@outlook.com>
2017-09-24 13:53:55 +0200

    Created CRUD client api calls for cities

--- 31.LessonThirtyOne-RestWebService/Frontend/src/api/CitiesApi.js
+++ 31.LessonThirtyOne-RestWebService/Frontend/src/api/CitiesApi.js
@@ -12,3 +12,32 @@ export const get = (cityId) => {
             url: `${apiUrl}/cities/${cityId}`
         });
 };
+
+export const getByName = (name) => {
+    return Api.apiCall({
+            url: `${apiUrl}/cities/${name}`
+        });
+};
+
+export const create = (city) => {
+    return Api.apiCall({
+        url: `${apiUrl}/cities`,
+        method: "POST",
+        body: JSON.stringify(city)
+    });
+};
+
+export const update = (city) => {
+    return Api.apiCall({
+        url: `${apiUrl}/cities/${city.id}`,
+        method: "PUT",
+        body: JSON.stringify(city)
+    });
+};
+
+export const deleteCity = (cityId) => {
+    return Api.apiCall({
+        url: `${apiUrl}/cities/${cityId}`,
+        method: "DELETE"
+    });
+};
