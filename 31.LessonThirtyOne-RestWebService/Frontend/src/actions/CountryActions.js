a1c892aa37121195b1db891efac1dbf850fe88ca
Aleksa Jovicic <aleksa.j@outlook.com>
2017-09-24 12:02:43 +0200

    Updated country actions

--- 31.LessonThirtyOne-RestWebService/Frontend/src/actions/CountryActions.js
+++ 31.LessonThirtyOne-RestWebService/Frontend/src/actions/CountryActions.js
@@ -1,16 +1,15 @@
-import * as countriesApi from "../Api/CountriesApi";
+import { getAll, get } from "../Api/CountriesApi";
 import * as action from "../constants";
 
 const getData = () => ({ type: action.FETCHING_COUNTRIES, isFetching: true });
-const getDataSuccess = (cities) => ({ type: action.FETCHING_COUNTRIES_SUCCESS, data: cities, isFetching: false });
-const getDataFailure = () => ({ type: action.FETCHING_COUNTRIES_FAILURE, isFetching: false });
+const getDataSuccess = (countries) => ({ type: action.FETCHING_COUNTRIES_SUCCESS, data: countries, isFetching: false });
+const getDataFailure = () => ({ type: action.FETCHING_COUNTRIES_FAILURE, error: true, isFetching: false });
 
 export const fetchCountries = () => {
     return (dispatch) => {
         dispatch(getData());
-        return countriesApi.getAll().
-            then(result =>
-                dispatch(getDataSuccess(result))).
-            catch(_ => dispatch(getDataFailure()));
+        return getAll()
+            .then(result => dispatch(getDataSuccess(result)))
+            .catch(_ => dispatch(getDataFailure()));
     };
 };
