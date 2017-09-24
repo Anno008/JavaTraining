cbdd287de0d89a85e5e8287c73dd88cdf7bcb771
Aleksa Jovicic <aleksa.j@outlook.com>
2017-09-24 15:06:37 +0200

    Updated App component and index.jsx

--- 31.LessonThirtyOne-RestWebService/Frontend/src/components/AppComponent.jsx
+++ 31.LessonThirtyOne-RestWebService/Frontend/src/components/AppComponent.jsx
@@ -3,10 +3,11 @@ import CitiesComponent from "./CitiesComponent";
 import CountriesComponent from "./CountriesComponent";
 import { fetchCities } from "../actions/CityActions";
 import { fetchCountries } from "../actions/CountryActions";
-
+import { store } from "../store/store";
 import "bulma/css/bulma.css";
 import "../css/app.css";
 import { BrowserRouter, Route, Link } from "react-router-dom";
+
 
 class AppComponent extends React.Component {
     constructor() {
@@ -21,8 +22,8 @@ class AppComponent extends React.Component {
                     <BrowserRouter>
                         <div className="container content">
                             <div className="links">
-                                <Link to="/cities">Cities</Link>
-                                <Link to="/countries">Countries</Link>                                
+                                <Link to="/cities" onClick={() => store.dispatch(fetchCities())}>Cities</Link>
+                                <Link to="/countries" onClick={() => store.dispatch(fetchCountries())}>Countries</Link>                                
                             </div>
                             <Route path="/cities" component={CitiesComponent} />
                             <Route path="/countries" component={CountriesComponent} />
--- 31.LessonThirtyOne-RestWebService/Frontend/src/index.jsx
+++ 31.LessonThirtyOne-RestWebService/Frontend/src/index.jsx
@@ -3,14 +3,8 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import AppComponent from "./components/AppComponent";
 import { store } from "./store/store";
-import { fetchCities } from "./actions/CityActions";
-import { fetchCountries } from "./actions/CountryActions";
-
 
 ReactDOM.render(
     <Provider store={store}>
         <AppComponent />
     </Provider>, document.getElementById("app"));
-
-store.dispatch(fetchCities());
-store.dispatch(fetchCountries());
