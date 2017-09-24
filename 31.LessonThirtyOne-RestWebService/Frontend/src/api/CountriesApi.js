ec0d5c1165bef509375e0a02aadcda0c6f95cc1f
Aleksa Jovicic <aleksa.j@outlook.com>
2017-09-24 13:57:42 +0200

    Created CRUD client functions for countries

--- 31.LessonThirtyOne-RestWebService/Frontend/src/api/CountriesApi.js
+++ 31.LessonThirtyOne-RestWebService/Frontend/src/api/CountriesApi.js
@@ -12,3 +12,39 @@ export const get = (countryId) => {
             url: `${apiUrl}/countries/${countryId}`
         });
 };
+
+
+export const getByName = (name) => {
+    return Api.apiCall({
+            url: `${apiUrl}/countries?name=${name}`
+        });
+};
+
+export const getByPopulation = (population, higher) => {
+    return Api.apiCall({
+            url: `${apiUrl}/countries?name=${name}&higherThan=${higher}`
+        });
+};
+
+export const create = (country) => {
+    return Api.apiCall({
+        url: `${apiUrl}/countries`,
+        method: "POST",
+        body: JSON.stringify(country)
+    });
+};
+
+export const update = (country) => {
+    return Api.apiCall({
+        url: `${apiUrl}/countries/${country.id}`,
+        method: "PUT",
+        body: JSON.stringify(country)
+    });
+};
+
+export const deleteCity = (countryId) => {
+    return Api.apiCall({
+        url: `${apiUrl}/countries/${countryId}`,
+        method: "DELETE"
+    });
+};
