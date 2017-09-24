ee3e5fb51952de966d9e3c6960d599737c7218e0
Aleksa Jovicic <aleksa.j@outlook.com>
2017-09-24 15:07:10 +0200

    Modified cities component to support search by name

--- 31.LessonThirtyOne-RestWebService/Frontend/src/components/CitiesComponent.jsx
+++ 31.LessonThirtyOne-RestWebService/Frontend/src/components/CitiesComponent.jsx
@@ -3,10 +3,13 @@ import { connect } from "react-redux";
 import CityComponent from "./CityComponent";
 import "bulma/css/bulma.css";
 import "../css/cities.css";
+import { fetchCitiesByName } from "../actions/CityActions";
 
 const CitiesComponent = (props) => {
     return (
         <div className="container">
+               <input className="input" type="text" placeholder="City name"
+                onChange={(e) => props.fetchCitiesByName(e.target.value)} />
             {props.citiesState.data ? props.citiesState.data.map(c =>
                 <div className="column is-half float" key={c.id}>
                     <CityComponent city={c} />
@@ -21,6 +24,7 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
+    fetchCitiesByName: (name) => dispatch(fetchCitiesByName(name)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CitiesComponent);
