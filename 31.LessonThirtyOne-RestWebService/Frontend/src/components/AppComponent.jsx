7b3649edb6709a077589df33373fe17ec50fb639
Aleksa Jovicic <aleksa.j@outlook.com>
2017-09-24 12:06:08 +0200

    implemented navigation between countries and cities component

--- 31.LessonThirtyOne-RestWebService/Frontend/src/components/AppComponent.jsx
+++ 31.LessonThirtyOne-RestWebService/Frontend/src/components/AppComponent.jsx
@@ -1,26 +1,35 @@
 import React from "react";
 import CitiesComponent from "./CitiesComponent";
+import CountriesComponent from "./CountriesComponent";
 import { fetchCities } from "../actions/CityActions";
+import { fetchCountries } from "../actions/CountryActions";
+
 import "bulma/css/bulma.css";
 import "../css/app.css";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 class AppComponent extends React.Component {
     constructor() {
         super();
-        fetchCities();
     }
 
     render() {
         return (
             <div>
                 <div className="nav"></div>
-                <div className="section">
-
-                    <div className="container content">
-                        <CitiesComponent />
-                    </div>
+                <div className="content">
+                    <BrowserRouter>
+                        <div className="container content">
+                            <div className="links">
+                                <Link to="/cities">Cities</Link>
+                                <Link to="/countries">Countries</Link>                                
+                            </div>
+                            <Route path="/cities" component={CitiesComponent} />
+                            <Route path="/countries" component={CountriesComponent} />
+                        </div>
+                    </BrowserRouter>
                 </div>
-                
+
             </div>
         );
     }
