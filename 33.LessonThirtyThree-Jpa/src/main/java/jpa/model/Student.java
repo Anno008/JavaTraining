f28b89481f46d3e8d67923d798d764f479283fa5
Aleksa Jovicic <aleksa.j@outlook.com>
2017-10-07 15:04:52 +0200

    Update to student model

--- 33.LessonThirtyThree-Jpa/src/main/java/jpa/model/Student.java
+++ 33.LessonThirtyThree-Jpa/src/main/java/jpa/model/Student.java
@@ -85,4 +85,53 @@ public class Student {
 	public void setExams(Set<Exam> exams) {
 		this.exams = exams;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		result = prime * result + ((card == null) ? 0 : card.hashCode());
+		result = prime * result + ((exams == null) ? 0 : exams.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((surname == null) ? 0 : surname.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		if (card == null) {
+			if (other.card != null)
+				return false;
+		} else if (!card.equals(other.card))
+			return false;
+		if (exams == null) {
+			if (other.exams != null)
+				return false;
+		} else if (!exams.equals(other.exams))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (surname == null) {
+			if (other.surname != null)
+				return false;
+		} else if (!surname.equals(other.surname))
+			return false;
+		return true;
+	}
 }
