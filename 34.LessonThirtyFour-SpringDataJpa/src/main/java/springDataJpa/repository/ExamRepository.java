054871b712b8aa5c440e6c3e8b60171f58de99fa
Aleksa Jovicic <aleksa.j@outlook.com>
2017-10-08 16:34:50 +0200

    Updated exam repository

--- 34.LessonThirtyFour-SpringDataJpa/src/main/java/springDataJpa/repository/ExamRepository.java
+++ 34.LessonThirtyFour-SpringDataJpa/src/main/java/springDataJpa/repository/ExamRepository.java
@@ -1,11 +1,22 @@
 package springDataJpa.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
 import springDataJpa.model.Exam;
+import springDataJpa.model.Subject;
 
 @Component
 public interface ExamRepository extends JpaRepository<Exam, Long> {
+	
+	public List<Exam> findBySubjectId(Long id);
+
+	public List<Exam> findByStudentId(Long studentId);
+
+	public double findStudentAvgGrade(Long studentId);
+
+	public List<Subject> findSubjectsByExamCount();
 
 }
