2e25db3b8fe25b1d580015cd5ae823c3eb28579a
Aleksa Jovicic <aleksa.j@outlook.com>
2017-10-09 22:47:13 +0200

    Added two methods to student repository

--- 34.LessonThirtyFour-SpringDataJpa/src/main/java/springDataJpa/repository/StudentRepository.java
+++ 34.LessonThirtyFour-SpringDataJpa/src/main/java/springDataJpa/repository/StudentRepository.java
@@ -1,5 +1,7 @@
 package springDataJpa.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
@@ -7,5 +9,9 @@ import springDataJpa.model.Student;
 
 @Component
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+	Student findByCardNumber(String cardNumber);
+
+	List<Student> findAllWithExams();
 
 }
