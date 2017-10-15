d1256aeaa161a8853e9b83d0842d55d601e1f5b6
Aleksa Jovicic <aleksa.j@outlook.com>
2017-10-15 18:06:04 +0200

    Updated user details service

--- 35.LessonThirtyFive-StudentsSecurityTest/src/main/java/studentsSecurityTest/service/UserDetailsServiceImpl.java
+++ 35.LessonThirtyFive-StudentsSecurityTest/src/main/java/studentsSecurityTest/service/UserDetailsServiceImpl.java
@@ -11,15 +11,25 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import studentsSecurityTest.model.user.SecurityAuthority;
 import studentsSecurityTest.model.user.SecurityUser;
+import studentsSecurityTest.model.user.SecurityUserAuthority;
+import studentsSecurityTest.repository.AuthorityRepository;
 import studentsSecurityTest.repository.UserRepository;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 	@Autowired
 	private UserRepository userRepository;
+
+	@Autowired
+	private AuthorityRepository authorityRepository;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	@Override
 	@Transactional
@@ -34,6 +44,23 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
 			return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
 					grantedAuthorities);
+		}
+	}
+
+	public SecurityUser register(SecurityUser securityUser, List<String> roles) {
+		if (userRepository.findByUsername(securityUser.getUsername()) != null)
+			return null;
+		else {
+			for (String role : roles) {
+				SecurityAuthority authority = authorityRepository.findByName(role);
+				SecurityUserAuthority sua = new SecurityUserAuthority();
+				sua.setAuthority(authority);
+				sua.setUser(securityUser);
+
+				securityUser.getUserAuthorities().add(sua);
+			}
+			securityUser.setPassword((passwordEncoder.encode(securityUser.getPassword())));
+			return userRepository.save(securityUser);
 		}
 	}
 
