ee4ea23658e221ccb2c08d861581ffa9a231afb0
Aleksa Jovicic <aleksa.j@outlook.com>
2017-10-15 15:17:40 +0200

    Added methods that return pages

--- 35.LessonThirtyFive-StudentsSecurityTest/src/main/java/studentsSecurityTest/repository/ExamRepository.java
+++ 35.LessonThirtyFive-StudentsSecurityTest/src/main/java/studentsSecurityTest/repository/ExamRepository.java
@@ -2,6 +2,8 @@ package studentsSecurityTest.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,8 +16,14 @@ public interface ExamRepository extends JpaRepository<Exam, Long> {
 	@Query("SELECT e FROM Exam e WHERE e.subject.id = :id")
 	public List<Exam> findBySubjectId(@Param("id") Long subjectId);
 
+	@Query("SELECT e FROM Exam e WHERE e.subject.id = :id")
+	public Page<Exam> findBySubjectId(@Param("id") Long subjectId, Pageable page);
+
 	@Query("SELECT e FROM Exam e WHERE e.student.id = :id")
 	public List<Exam> findByStudentId(@Param("id") Long studentId);
+
+	@Query("SELECT e FROM Exam e WHERE e.student.id = :id")
+	public Page<Exam> findByStudentId(@Param("id") Long studentId, Pageable page);
 
 	@Query("SELECT AVG(e.grade) from Exam e where e.student.id = :id")
 	public double findStudentAvgGrade(@Param("id") Long studentId);
