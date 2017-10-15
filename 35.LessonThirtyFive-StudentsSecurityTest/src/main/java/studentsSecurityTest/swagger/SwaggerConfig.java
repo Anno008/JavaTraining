c3faf0b8ff4e76411ec1922a499e47fbd9cdf7b8
Aleksa Jovicic <aleksa.j@outlook.com>
2017-10-15 19:32:03 +0200

    Added swagger config

---
+++ 35.LessonThirtyFive-StudentsSecurityTest/src/main/java/studentsSecurityTest/swagger/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package studentsSecurityTest.swagger;
+
+import org.springframework.context.annotation.Bean;
+import static springfox.documentation.builders.PathSelectors.regex;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("studentsSecurityTest.controller"))
+				.paths(PathSelectors.ant("/api/*")).build();
+	}
+}
