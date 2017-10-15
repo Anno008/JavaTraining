3513ef31e3b5dc9c2f86bee4f8ab5447597ce4a6
Aleksa Jovicic <aleksa.j@outlook.com>
2017-10-15 15:29:01 +0200

    Created the dto classes

---
+++ 35.LessonThirtyFive-StudentsSecurityTest/src/main/java/studentsSecurityTest/dto/SubjectDTO.java
@@ -0,0 +1,64 @@
+package studentsSecurityTest.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import studentsSecurityTest.model.Subject;
+
+public class SubjectDTO {
+	private Long id;
+	private String name;
+	private int espb;
+
+	private List<ExamDTO> exams = new ArrayList<ExamDTO>();
+
+	public SubjectDTO() {
+
+	}
+
+	public SubjectDTO(Long id, String name, int espb, List<ExamDTO> exams) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.espb = espb;
+		this.exams = exams;
+	}
+
+	public SubjectDTO(Subject subject) {
+		this.id = subject.getId();
+		this.name = subject.getName();
+		this.espb = subject.getEspb();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getEspb() {
+		return espb;
+	}
+
+	public void setEspb(int espb) {
+		this.espb = espb;
+	}
+
+	public List<ExamDTO> getExams() {
+		return exams;
+	}
+
+	public void setExams(List<ExamDTO> exams) {
+		this.exams = exams;
+	}
+}
