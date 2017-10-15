8d8543f13b517927603e09b829b57b66c48ea181
Aleksa Jovicic <aleksa.j@outlook.com>
2017-10-15 15:17:54 +0200

    Created the service classes

---
+++ 35.LessonThirtyFive-StudentsSecurityTest/src/main/java/studentsSecurityTest/service/ExamService.java
@@ -0,0 +1,53 @@
+package studentsSecurityTest.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+import studentsSecurityTest.model.Exam;
+import studentsSecurityTest.repository.ExamRepository;
+
+@Component
+public class ExamService {
+	@Autowired
+	private ExamRepository examRepository;
+
+	public List<Exam> findAll() {
+		return examRepository.findAll();
+	}
+
+	public Page<Exam> findAll(Pageable page) {
+		return examRepository.findAll(page);
+	}
+
+	public Exam findOne(Long id) {
+		return examRepository.findOne(id);
+	}
+
+	public Exam save(Exam exam) {
+		return examRepository.save(exam);
+	}
+
+	public void remove(Long id) {
+		examRepository.delete(id);
+	}
+
+	public List<Exam> findByStudentId(Long studentId) {
+		return examRepository.findByStudentId(studentId);
+	}
+
+	public Page<Exam> findByStudentId(Long studentId, Pageable page) {
+		return examRepository.findByStudentId(studentId, page);
+	}
+
+	public List<Exam> findBySubjectId(Long subjectId) {
+		return examRepository.findBySubjectId(subjectId);
+	}
+
+	public Page<Exam> findBySubjectId(Long subjectId, Pageable page) {
+		return examRepository.findBySubjectId(subjectId, page);
+	}
+}
---
+++ 35.LessonThirtyFive-StudentsSecurityTest/src/main/java/studentsSecurityTest/service/SubjectService.java
@@ -0,0 +1,42 @@
+package studentsSecurityTest.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+import studentsSecurityTest.model.Subject;
+import studentsSecurityTest.repository.SubjectRepository;
+
+@Component
+public class SubjectService {
+	@Autowired
+	private SubjectRepository subjectRepository;
+
+	@Autowired
+	private ExamService examService;
+
+	public List<Subject> findAll() {
+		return subjectRepository.findAll();
+	}
+
+	public Page<Subject> findAll(Pageable page) {
+		return subjectRepository.findAll(page);
+	}
+
+	public Subject findOne(Long id) {
+		return subjectRepository.findOne(id);
+	}
+
+	public Subject save(Subject subject) {
+		return subjectRepository.save(subject);
+	}
+
+	public void remove(Long id) {
+		if (examService.findBySubjectId(id).isEmpty()) {
+			subjectRepository.delete(id);
+		}
+	}
+}
