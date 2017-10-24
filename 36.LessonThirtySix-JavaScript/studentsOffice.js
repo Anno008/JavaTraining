90a3f5db89f4a96b748d4006c7c9bd291eaccbab
Aleksa Jovicic <aleksa.j@outlook.com>
2017-10-24 18:36:43 +0200

    Created studentsOffice.js

---
+++ 36.LessonThirtySix-JavaScript/studentsOffice.js
@@ -0,0 +1,39 @@
+class StudentsOffice {
+    constructor() {
+        this.students = [];
+        this.subjects = [];
+        this.exams = [];
+    }
+
+    addStuednt(student) {
+        this.students.push(student);
+    }
+
+    addSubject(subject) {
+        this.subjects.push(subject);
+    }
+
+    takeAnExam(studentIndex, exam) {
+        const student = this.students[studentIndex];
+        if (student === undefined) {
+            console.log(`Student with the provided index of: ${studentIndex} doesn't exist`);
+            return;
+        }
+        student.addExam(exam);
+    }
+
+    invalidateAnExam(studentIndex, examIndex) {
+        const student = this.students[studentIndex];
+        if (student === undefined) {
+            console.log(`Student with the provided index of: ${studentIndex} doesn't exist`);
+            return;
+        }
+        if (student.exams.length > 0) {
+            student.exams.splice(examIndex, 1);
+        }
+    }
+
+    getStudentsInfo(){
+        return this.students.map(s => `${s}`);
+    }
+}
