801f869762756b0b18adbab3eb5d021cba2cab0c
Aleksa Jovicic <aleksa.j@outlook.com>
2017-11-26 13:39:01 +0100

    Finished ExamController

--- 39.LessonThirtyNine-ClientServerAngular/server/School/src/main/java/school/controller/ExamController.java
+++ 39.LessonThirtyNine-ClientServerAngular/server/School/src/main/java/school/controller/ExamController.java
@@ -1,5 +1,45 @@
 package school.controller;
 
-public class ExamController {
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import school.dto.ExamDTO;
+import school.model.Exam;
+import school.service.ExamService;
+
+@RestController
+public class ExamController {
+	@Autowired
+	private ExamService examService;
+
+	@GetMapping(value = "api/exams")
+	public ResponseEntity<List<ExamDTO>> getExams() {
+		return new ResponseEntity<List<ExamDTO>>(
+				examService.findAll().stream().map(ExamDTO::new).collect(Collectors.toList()), HttpStatus.OK);
+	}
+
+	@GetMapping(value = "api/exams/{id}")
+	public ResponseEntity<ExamDTO> getExam(@PathVariable Long id) {
+		Exam exam = examService.findOne(id);
+		return exam == null ? new ResponseEntity<>(HttpStatus.NOT_FOUND)
+				: new ResponseEntity<ExamDTO>(new ExamDTO(exam), HttpStatus.OK);
+	}
+
+	@DeleteMapping(value = "api/exams/{id}")
+	public ResponseEntity<HttpStatus> delete(@PathVariable Long id) {
+		Exam exam = examService.findOne(id);
+		if (exam == null) 
+			return new ResponseEntity<HttpStatus>(HttpStatus.NOT_FOUND);
+		
+		examService.remove(id);
+		return new ResponseEntity<HttpStatus>(HttpStatus.OK);
+	}
 }
