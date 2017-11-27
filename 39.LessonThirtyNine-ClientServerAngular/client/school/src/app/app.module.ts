9cccfa862462f43e27b74b7697bdd7b754fbd47a
Aleksa Jovicic <aleksa.j@outlook.com>
2017-11-27 22:29:40 +0100

    Created empty angular project

---
+++ 39.LessonThirtyNine-ClientServerAngular/client/school/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule} from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+const appRoutes: Routes = [
+  // { path: 'students', component: StudentsComponent },
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    // StudentsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {
+        enableTracing: false
+      }
+    )
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
