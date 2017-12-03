60461fd5549a860b0ef7bb4eb025952732eeba9e
Aleksa Jovicic <aleksa.j@outlook.com>
2017-12-03 22:53:40 +0100

    Updated app.component.html and imported some stuff in the app module

--- 39.LessonThirtyNine-ClientServerAngular/client/school/src/app/app.module.ts
+++ 39.LessonThirtyNine-ClientServerAngular/client/school/src/app/app.module.ts
@@ -2,9 +2,15 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpModule } from "@angular/http";
 import { RouterModule, Routes } from "@angular/router";
-import { FormsModule} from "@angular/forms";
+import { FormsModule } from "@angular/forms";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
+
+import { AuthenticationService } from "./services/authentication-service.service";
+import { JwtUtilsService } from "./services/jwt-utils.service";
+import { TokenInterceptorService } from "./services/token-interceptor.service";
+import { CanActivateAuthGuard } from "./services/can-activate-auth.guard";
 
 const appRoutes: Routes = [
   // { path: 'students', component: StudentsComponent },
@@ -26,7 +32,16 @@ const appRoutes: Routes = [
       }
     )
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptorService,
+      multi: true
+    },
+    AuthenticationService,
+    CanActivateAuthGuard,
+    JwtUtilsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
