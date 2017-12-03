b37c770203c10e21e2b62d292afa46bd9aa4b55f
Aleksa Jovicic <aleksa.j@outlook.com>
2017-12-03 19:51:34 +0100

    Updated security user

--- 39.LessonThirtyNine-ClientServerAngular/server/School/src/main/java/school/model/SecurityUser.java
+++ 39.LessonThirtyNine-ClientServerAngular/server/School/src/main/java/school/model/SecurityUser.java
@@ -1,5 +1,51 @@
 package school.model;
 
-public class SecurityUser {
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+public class SecurityUser {
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String username;
+
+	private String password;
+
+	@Enumerated(EnumType.STRING)
+	private Role role;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
 }
