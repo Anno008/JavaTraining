714aee525709ac1dbbab7eb8e0adfb152342f762
Aleksa Jovicic <aleksa.j@outlook.com>
2017-12-07 22:09:10 +0100

    Created user controller with login and register endpoints

---
+++ 40.LessonFourty-PcShop/backend/src/main/java/pcShop/controller/UserController.java
@@ -0,0 +1,67 @@
+package pcShop.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import pcShop.dto.LoginDTO;
+import pcShop.dto.TokenDTO;
+import pcShop.dto.UserDTO;
+import pcShop.model.security.Role;
+import pcShop.model.security.SecurityUser;
+import pcShop.security.TokenUtils;
+import pcShop.service.UserDetailsServiceImpl;
+
+@RestController
+public class UserController {
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private UserDetailsServiceImpl userDetailsService;
+
+	@Autowired
+	private TokenUtils tokenUtils;
+
+	@PostMapping(value = "/api/login")
+	public ResponseEntity<TokenDTO> login(@RequestBody LoginDTO loginDTO) {
+		try {
+			final UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+					loginDTO.getUsername(), loginDTO.getPassword());
+			final Authentication authentication = authenticationManager.authenticate(token);
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+			final UserDetails details = userDetailsService.loadUserByUsername(loginDTO.getUsername());
+			final String genToken = tokenUtils.generateToken(details);
+
+			return new ResponseEntity<>(new TokenDTO(genToken), HttpStatus.OK);
+		} catch (Exception ex) {
+			return new ResponseEntity<>(new TokenDTO(""), HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	@RequestMapping(value = "/api/register", method = RequestMethod.POST)
+	public ResponseEntity<UserDTO> register(@RequestBody UserDTO userDTO) {
+		SecurityUser user = new SecurityUser();
+		user.setUsername(userDTO.getUsername());
+		user.setPassword(userDTO.getPassword());
+		user.setRole(Role.User);
+
+		user = userDetailsService.register(user);
+
+		if (user != null) {
+			return new ResponseEntity<>(new UserDTO(user), HttpStatus.CREATED);
+		} else {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
+}
