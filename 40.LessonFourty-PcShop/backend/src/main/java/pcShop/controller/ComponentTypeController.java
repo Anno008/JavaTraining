a9c4f1aa9fde679044469219e60f8f06fbd69cde
Aleksa Jovicic <aleksa.j@outlook.com>
2017-12-07 22:52:48 +0100

    Created Component type controller

---
+++ 40.LessonFourty-PcShop/backend/src/main/java/pcShop/controller/ComponentTypeController.java
@@ -0,0 +1,24 @@
+package pcShop.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RestController;
+
+import pcShop.dto.ComponentTypeDTO;
+import pcShop.service.ComponentTypeService;
+
+@RestController
+public class ComponentTypeController {
+	@Autowired
+	private ComponentTypeService componentTypeService;
+	
+	@PreAuthorize("isAuthorized()")
+	public ResponseEntity<List<ComponentTypeDTO>> get() {
+		return new ResponseEntity<List<ComponentTypeDTO>>(componentTypeService.findAll().stream().map(ComponentTypeDTO::new).collect(Collectors.toList()), HttpStatus.OK);
+	}
+}
