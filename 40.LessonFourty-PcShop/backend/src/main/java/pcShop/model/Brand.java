3d045f9ee0ecb1f2cdc629dad397e7e1ead5278d
Aleksa Jovicic <aleksa.j@outlook.com>
2017-12-07 20:49:27 +0100

    Created a java rest api application

---
+++ 40.LessonFourty-PcShop/backend/src/main/java/pcShop/model/Brand.java
@@ -0,0 +1,33 @@
+package pcShop.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(catalog = "pcShop", name = "brand")
+public class Brand {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
